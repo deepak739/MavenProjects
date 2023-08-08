1831b2c5b6baf46cfb144e1f0f4cbf4a06cf5abd
deepak739 <46920411+deepak739@users.noreply.github.com>
2023-08-08 08:24:56 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package mythings.KeepLearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
